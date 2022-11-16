2af34b1bd50831f09c5d6c3584f4df99117b6900
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-15 18:16:48 -0700

    import a bodyparser and attach it to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.bu4mnvm.mongodb.net/?retryWrites=true&w=majority'
