b2bf3468c1168c0584a3a7543b6adbc68be6f2db
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-16 17:47:15 -0700

    Add error handling for email and password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,10 +8,16 @@ router.post('/signup', async (req, res) => {
   const { email, password } = req.body;
 
   // create a new instance of a user
-  const user = new User({ email, password });
-  await user.save();
+  try {
 
-  res.send('You made a post request');
+    const user = new User({ email, password });
+    await user.save();
+
+    res.send('You made a post request');
+  } catch (err) {
+    //  you can write either err.message or a custom message of your like!
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
