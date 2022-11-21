29e13d3f336b30d07f5e228fcb8836e76f5c0242
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-20 23:58:05 -0700

    Change the status code to 422

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -31,7 +31,7 @@ router.post('/signin', async (req, res) => {
 
   const user = await User.findOne({ email });
   if (!user) {
-    return res.status(404).send({ error: 'Invalid password or email' });
+    return res.status(422).send({ error: 'Invalid password or email' });
   }
 
   try {
