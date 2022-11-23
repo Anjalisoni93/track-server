4fadf7da073a9983b13b17e99d14ae4b6f3eaec6
Anjali Soni <anjalipatadiya@gmail.com>
2022-11-23 15:44:40 -0700

    Define points schema with various diffrent objects inside

--- src/models/Track.js
+++ src/models/Track.js
@@ -1,7 +1,15 @@
 const mongoose = require ('mongoose');
 
 const pointSchema = new mongoose.Schema({
-  
+  timestamp: Number,
+  coords: {
+    latitude: Number,
+    longitude: Number,
+    altitude: Number,
+    accuracy: Number,
+    heading: Number,
+    speed: Number
+  }
 });
 
 const trackSchema = new mongoose.Schema({
